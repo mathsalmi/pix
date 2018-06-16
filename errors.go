19c9546edd3a0bc8bbe612be47ee0c0393b4c7b9
Matheus Salmi <mathsalmi@gmail.com>
2018-06-16 12:03:07 -0300

    Improve errors doc

--- errors.go
+++ errors.go
@@ -7,12 +7,22 @@ import (
 
 // Error codes returned by the download process
 var (
-	ErrInvalidExtension  = errors.New("pix: invalid extension")
-	ErrOptionNotProvided = errors.New("pix: cannot parse values because this option was not provided")
-	ErrInvalidDimensions = errors.New("pix: invalid image dimensions")
-	ErrEffectNotApplied  = errors.New("pix: effect not applied")
+	ErrInvalidExtension = errors.New("pix: invalid extension")
 )
 
+// Error codes for options parsing
+var (
+	ErrOptionNotProvided   = errors.New("pix: option not provided")
+	ErrInvalidOptionValues = errors.New("pix: cannot parse option because the provided values are invalid")
+	ErrInvalidDimensions   = errors.New("pix: invalid image dimensions")
+)
+
+// Error codes for effects/transformation process
+var (
+	ErrEffectNotApplied = errors.New("pix: effect not applied")
+)
+
+// fail writes the string representation of an error to the response
 func fail(w http.ResponseWriter, err error, code int) {
 	http.Error(w, err.Error(), code)
 }
